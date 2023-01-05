0c84272915bf5cd79f4c43154fd5f470bf5a5e60
verdantpath <m_dittmann@yahoo.com>
2023-01-04 22:33:03 -0800

    Add social icons

---
+++ data/socialItems.js
@@ -0,0 +1,21 @@
+const socialItems = [
+  {
+    icon: 'mdi-github',
+    title: 'GitHub',
+    to: 'https://github.com/verdantpath',
+    exact: 'false'
+  },
+  {
+    icon: 'mdi-linkedin',
+    title: 'LinkedIn',
+    to: 'https://shorturl.at/bcrX6',
+    exact: 'false'
+  },
+  {
+    icon: 'mdi-twitter',
+    title: 'Twitter',
+    to: 'https://twitter.com/verdantpath',
+    exact: 'false'
+  }
+]
+export default socialItems
