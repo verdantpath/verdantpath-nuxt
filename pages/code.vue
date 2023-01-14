5a3608154f518142e964c025fc971af1c82037f0
verdantpath <m_dittmann@yahoo.com>
2023-01-14 12:24:18 -0800

    Update code example single template

--- pages/code.vue
+++ pages/code.vue
@@ -1,20 +1,7 @@
 <template>
   <div>
     <h1>Code</h1>
-    <v-container class="mb-6">
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 lg3 v-for="code in codes" :key="code.name">
-          <v-card class="portfolio-card">
-            <v-responsive class="pt-4">
-              <img :src="code.imageThumbnail" class="portfolio-thumb">
-            </v-responsive>
-            <v-card-text>
-              <div><nuxt-link :to="`codes/${code.id}`">{{ code.name }}</nuxt-link></div>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
+    <nuxt-child />
   </div>
 </template>
 <script>
@@ -26,6 +13,9 @@ import codes from '@/data/code'
   }
 </script>
 <style scoped lang="scss">
+h1 {
+  display: none;
+}
 .v-card {
   background: rgba(255,255,255,0.3);
   color: #000;
