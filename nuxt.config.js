a7491614c7ce103b80618b15a7ad3afc92c6ca6a
verdantpath <m_dittmann@yahoo.com>
2023-02-25 13:13:31 -0800

    Add project config files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,11 @@
 import colors from 'vuetify/es5/util/colors'
 
-export default {
+export default {  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  // ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - verdantpath-nuxt',
